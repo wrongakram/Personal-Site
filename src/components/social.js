75c2fea3fe1788b1f152d7cee895e40063b76f36
akram <akramkhalid12@gmail.com>
2019-03-27 23:18:15 -0500

    updated github url

--- src/components/social.js
+++ src/components/social.js
@@ -1,22 +1,25 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
-import {TimelineLite,} from "gsap";
-import { faDribbble, faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { TimelineLite } from 'gsap'
+import {
+  faDribbble,
+  faGithub,
+  faLinkedin,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-library.add(faDribbble, faGithub, faLinkedin, faInstagram);
-
-
+library.add(faDribbble, faGithub, faLinkedin, faInstagram)
 
 const SocialLinks = styled.div`
-margin-top: 50px;
-position: relative;
-padding-left: 100px;
-z-index: 10;
-@media (max-width: 600px) {
- padding-left: 5%;
-}
+  margin-top: 50px;
+  position: relative;
+  padding-left: 100px;
+  z-index: 10;
+  @media (max-width: 600px) {
+    padding-left: 5%;
+  }
   ul {
     margin: 0;
     padding: 0;
@@ -41,43 +44,48 @@ z-index: 10;
   }
 `
 
-
-
 const socialsArray = [
-  {url: "https://dribbble.com/Arxaa",  icon: "dribbble"},
-  {url: "https://github.com/akramk22", icon: "github"},
-  {url: "https://www.linkedin.com/in/akram-khalid-12b977142/", icon: "linkedin"},
-  {url: "https://www.instagram.com/wrongakram",  icon: "instagram"},
+  { url: 'https://dribbble.com/Arxaa', icon: 'dribbble' },
+  { url: 'https://github.com/wrongakram', icon: 'github' },
+  {
+    url: 'https://www.linkedin.com/in/akram-khalid-12b977142/',
+    icon: 'linkedin',
+  },
+  { url: 'https://www.instagram.com/wrongakram', icon: 'instagram' },
 ]
 
 class SocialIcons extends Component {
   constructor(props) {
-    super(props);
-    this.socials = [];
-// reference to the animation
-    this.myTween = null;
+    super(props)
+    this.socials = []
+    // reference to the animation
+    this.myTween = null
   }
-  componentDidMount(){
-    this.myTween = new TimelineLite();
-    this.myTween
-    .staggerFrom(this.socials, .2, { delay: 1,scale: 0, autoAlpha: 0 }, 0.1) //animate
+  componentDidMount() {
+    this.myTween = new TimelineLite()
+    this.myTween.staggerFrom(
+      this.socials,
+      0.2,
+      { delay: 1, scale: 0, autoAlpha: 0 },
+      0.1
+    ) //animate
   }
   render() {
     return (
       <SocialLinks>
         <ul>
-          {socialsArray.map( (social, index) => {
+          {socialsArray.map((social, index) => {
             return (
               <li
                 key={`social-${index}`}
-                ref={ li => this.socials[index] = li }
-                >
-                <a href={social.url}><FontAwesomeIcon icon={['fab', `${social.icon}`]} /></a>
+                ref={li => (this.socials[index] = li)}
+              >
+                <a href={social.url}>
+                  <FontAwesomeIcon icon={['fab', `${social.icon}`]} />
+                </a>
               </li>
             )
-          } )
-
-          }
+          })}
         </ul>
       </SocialLinks>
     )
