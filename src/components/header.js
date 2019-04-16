b324e36b51ad009f6ed6389847c48ef5c2754390
akram <akramkhalid12@gmail.com>
2019-04-15 21:30:23 -0500

    Removed header animation and page transitions

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,11 @@
-import React, {Component} from 'react';
-import styled, {css} from 'styled-components';
-import AniLink from "gatsby-plugin-transition-link/AniLink";
-import {TimelineLite, Expo,} from "gsap";
+import React, { Component } from 'react'
+import styled, { css } from 'styled-components'
+import { Link } from 'gatsby'
 
 const CursorItem = styled.div`
   --size: 6px;
   height: var(--size);
-  width:  var(--size);
+  width: var(--size);
   border-radius: 50%;
   position: absolute;
   /* z-index: 999; */
@@ -15,26 +14,29 @@ const CursorItem = styled.div`
   top: -100px;
   left: -100px;
 
-  ${props => props.shadow && css`
-    opacity: .75;
-    background: #efefff;
+  ${props =>
+    props.shadow &&
+    css`
+      opacity: 0.75;
+      background: #efefff;
       --size: 50px;
-    transition: top .25s, left .25s, width .5s, height .5s, background .35s;
-    transition-timing-function: ease-out;
-    @media (max-width: 800px) {
-      display: none !important;
-    }
-  `}
+      transition: top 0.25s, left 0.25s, width 0.5s, height 0.5s,
+        background 0.35s;
+      transition-timing-function: ease-out;
+      @media (max-width: 800px) {
+        display: none !important;
+      }
+    `}
 
-  ${props => props.dot && css`
-    background: #000;
-    transition: width .25s, height .25s;
-    @media (max-width: 800px) {
-      display: none !important;
-    }
-  `}
-
-
+  ${props =>
+    props.dot &&
+    css`
+      background: #000;
+      transition: width 0.25s, height 0.25s;
+      @media (max-width: 800px) {
+        display: none !important;
+      }
+    `}
 `
 
 const NavItem = styled.li`
@@ -42,7 +44,6 @@ const NavItem = styled.li`
   padding: 0;
   list-style: none;
   a {
-    margin-top: 10px;
     text-decoration: none;
     font-size: 12px;
     text-transform: uppercase;
@@ -50,72 +51,72 @@ const NavItem = styled.li`
     font-family: 'Lato';
     font-weight: 700;
   }
-  `
+`
 
-  const NavWrapper = styled.div`
+const NavWrapper = styled.div`
   padding: 25px 100px;
-  marginBottom: 1.45rem;
+  marginbottom: 1.45rem;
   position: fixed;
   width: 100%;
   height: 70px;
-  opacity: 0;
-  transform: translateY(10px);
   background: #fff;
   top: 0;
+  border-bottom: 1px solid #f7f7f7;
   @media (max-width: 600px) {
     padding: 10px 5%;
   }
-  `
-
+`
 
 class Header extends Component {
   constructor(props) {
-    super(props);
-    this.hoverLink = null;
-    this.hoverLink2 = null;
-    this.myTween = null;
-    this.header = null;
+    super(props)
+    this.hoverLink = null
+    this.hoverLink2 = null
+    this.myTween = null
+    this.header = null
   }
 
-
-  componentDidMount(){
-    this.myTween = new TimelineLite();
-    this.myTween
-    .to(this.header, .6, {opacity: 1, delay: .8 ,ease: Expo.easeOut, x: 0, y: 0})
-
-    window.onmousemove = (e) => {
-      this.cursorShadow.style.left = e.pageX + 'px';
-      this.cursorShadow.style.top = e.pageY + 'px';
-      this.cursorDot.style.left = e.pageX + 'px';
-      this.cursorDot.style.top = e.pageY + 'px';
+  componentDidMount() {
+    window.onmousemove = e => {
+      this.cursorShadow.style.left = e.pageX + 'px'
+      this.cursorShadow.style.top = e.pageY + 'px'
+      this.cursorDot.style.left = e.pageX + 'px'
+      this.cursorDot.style.top = e.pageY + 'px'
     }
     this.hoverLink.onmouseover = () => {
-     this.cursorShadow.classList.add('active');
-     this.cursorDot.classList.add('active');
-  }
+      this.cursorShadow.classList.add('active')
+      this.cursorDot.classList.add('active')
+    }
 
-     this.hoverLink.onmouseleave = () => {
-     this.cursorShadow.classList.remove('active');
-     this.cursorDot.classList.remove('active');
-  }
-
+    this.hoverLink.onmouseleave = () => {
+      this.cursorShadow.classList.remove('active')
+      this.cursorDot.classList.remove('active')
+    }
 
     this.hoverLink2.onmouseover = () => {
-     this.cursorShadow.classList.add('active');
-     this.cursorDot.classList.add('active');
-  }
+      this.cursorShadow.classList.add('active')
+      this.cursorDot.classList.add('active')
+    }
 
-     this.hoverLink2.onmouseleave = () => {
-     this.cursorShadow.classList.remove('active');
-     this.cursorDot.classList.remove('active');
+    this.hoverLink2.onmouseleave = () => {
+      this.cursorShadow.classList.remove('active')
+      this.cursorDot.classList.remove('active')
+    }
   }
-}
 
   render() {
     return (
-      <NavWrapper ref={a => this.header = a}>
-        <CursorItem shadow ref={div => this.cursorShadow = div} className="cursor-shadow"></CursorItem>
-        <CursorItem dot ref={div => this.cursorDot = div} className="cursor-dot"></CursorItem>
+      <NavWrapper ref={a => (this.header = a)}>
+        <CursorItem
+          shadow
+          ref={div => (this.cursorShadow = div)}
+          className="cursor-shadow"
+        />
+        <CursorItem
+          dot
+          ref={div => (this.cursorDot = div)}
+          className="cursor-dot"
+        />
         <div
           style={{
             margin: `0 auto`,
@@ -124,15 +125,21 @@ class Header extends Component {
             justifyContent: 'space-between',
           }}
         >
-        <NavItem ref={a => this.hoverLink2 = a} ><AniLink cover bg="#000" to="/" duration={1}>A.</AniLink></NavItem>
+          <NavItem ref={a => (this.hoverLink2 = a)}>
+            <Link to="/">A.</Link>
+          </NavItem>
           <div>
-            <ul style={{
-                    padding: 0,
-                    margin: 0,
-                    display: 'flex',
-                    alignItems: 'center'
-                  }} >
-              <NavItem ref={a => this.hoverLink = a} ><AniLink cover bg="#000" to="/about" duration={1}>about</AniLink></NavItem>
+            <ul
+              style={{
+                padding: 0,
+                margin: 0,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <NavItem ref={a => (this.hoverLink = a)}>
+                <Link to="/about">about</Link>
+              </NavItem>
             </ul>
           </div>
         </div>
@@ -140,6 +147,5 @@ class Header extends Component {
     )
   }
 }
-
 
 export default Header
